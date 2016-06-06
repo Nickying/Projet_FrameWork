a15de985071588b90c3e4feca68851415fa7aae1
drexchelastom <drexchel.astom@gmail.com>
2016-06-06 22:42:25 +0200

    Bootstrap
    Ajout de bootstrap

--- angular/app/accueil/accueil.component.ts
+++ angular/app/accueil/accueil.component.ts
@@ -2,7 +2,13 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'my-accueil',
-  template: '<div id="menu"><h1>iDeas</h1><div class="button"><button name="btnCreerCompte">Créer un compte</button><button name="btnConnecter">Se connecter</button></div></div>'+
-      '<p>Plate Forme de partage d\'idées</p>'
+  template: '<div id="menu">' +
+				'<h1>iDeas</h1>' + 
+				'<div class="button">' +
+					'<button name="btnCreerCompte">Créer un compte</button>' +
+					'<button name="btnConnecter">Se connecter</button>' +
+				'</div>' +
+			'</div>' +
+			'<p>Plate Forme de partage d\'idées</p>'
 })
 export class AccueilComponent{ }
---
+++ angular/index.php
@@ -0,0 +1,11 @@
+<script>
+$.ajax({
+       url : 'url',
+       type : 'POST',
+       data : 'data1=' + data1 + '&data2=' + data2,
+       dataType : 'json',
+       success : function(json){ 
+			
+       }
+    });
+</script>
