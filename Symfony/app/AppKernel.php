4428294110e872ed6c7a706cf40fa08bf4e34d91
naturus <Guillaume@client-172-18-93-0.clients.u-psud.fr>
2016-06-07 13:25:35 +0200

    dfhidfhidfdihdhi

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new RestBundle\RestBundle(),
+            new OCUserBundle\OCUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- Symfony/src/RestBundle/Controller/RestController.php
+++ Symfony/src/RestBundle/Controller/RestController.php
@@ -4,22 +4,87 @@ namespace RestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
-class RestController extends Controller
-{
+class RestController extends Controller {
     
     
     
-    public function indexAction()
-    {
+    public function indexAction() {
         $PDO = new \PDO('mysql:host=localhost;dbname=Blog','root','root');
+	
+	
         
         $sth = $PDO->prepare('SELECT *
                                 FROM Internaute');
+	
         
         $sth->execute();
         
         
-        return new JsonResponse($sth->fetchAll());
+        return new JsonResponse($sth->fetchAll(\PDO::FETCH_ASSOC));
+    }
+    
+    public function testpseudoAction(Request $request) {
+        $PDO = new \PDO('mysql:host=localhost;dbname=Blog','root','root');
+	$pseudo=$request->request->get('pseudo');
+	
+        
+        $sth = $PDO->prepare("SELECT count(pseudo) as existe
+                                FROM Internaute 
+                                where pseudo='$pseudo'");
+	
+        
+        $sth->execute();
+        
+        
+        return new JsonResponse($sth->fetchAll(\PDO::FETCH_ASSOC));
+    }
+
+
+    public function adduserAction(Request $request) {
+        try {
+        $pseudo=$request->request->get('pseudo');
+        $nom=$request->request->get('nom');
+        $prenom=$request->request->get('prenom');
+        $email=$request->request->get('email');
+        $mdp=$request->request->get('mdp');
+        $PDO = new \PDO('mysql:host=localhost;dbname=Blog','root','root');
+	$PDO->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_WARNING);
+        $PDO->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE,\PDO::FETCH_OBJ);
+	
+        
+
+
+        $sth = $PDO->prepare("INSERT INTO Internaute (pseudo,mdp,nom,prenom,email,dateinscription) VALUES('$pseudo',password('$mdp'),'$nom','$prenom','$email',Now());");
+        
+	
+        
+        $sth->execute();
+        }
+        
+        catch (PDOException $e) {
+    echo 'Échec lors de la connexion : ' . $e->getMessage();
+}
+        
+        
+        return '$sth->rowCount()';
+    }
+    
+    public function arAction() {
+        
+        $PDO = new \PDO('mysql:host=localhost;dbname=Blog','root','root');
+	
+	
+        
+        $sth = $PDO->prepare('SELECT *
+                                FROM Internaute');
+	
+        
+        $sth->execute();
+        
+        
+        return new JsonResponse($sth->fetchAll(\PDO::FETCH_ASSOC));
     }
 }
